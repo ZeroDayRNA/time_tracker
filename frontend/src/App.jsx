5dbbf8e86164fd477f47ea31c42396085a361c9e
ZeroDayRNA <skinekopia@gmail.com>
2024-04-14 10:12:53 -0700

    Created boilerplate for Login, NotFound, Calendar, and Register pages and routed them in App.jsx. Defined Calendar as a protected route.'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,55 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Calendar from './pages/Calendar'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to='/login'/>
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register/>
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <Calendar></Calendar>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/login'
+          element={<Login/>}
+        />
+        <Route
+          path='/register'
+          element={<RegisterAndLogout/>}
+        />
+        <Route
+          path='/logout'
+          element={<Logout/>}
+        />
+        <Route
+          path='*'
+          element={<NotFound/>}
+        />                    
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
